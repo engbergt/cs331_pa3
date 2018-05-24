af6b32169d5de254060fad1670f6157c399be67f
Tarren Engberg <tarren@outlook.com>
2018-05-24 15:24:59 -0700

    List processed down to unique words.

--- preprocessor.py
+++ preprocessor.py
@@ -5,12 +5,14 @@ import string
 with open('myTester.txt') as textFile:
     content = textFile.readlines()
 
+
 ### Remove punctuation & apostrophes ###
 
 noPuncList = []
 
 for ln in content: 
     noPuncList.append(ln.translate(None, string.punctuation))
+
 
 ### Make all lowercase ###
 
@@ -19,12 +21,31 @@ lowerNoPuncList = []
 for ln in noPuncList: 
     lowerNoPuncList.append(ln.lower())
 
-for ln in lowerNoPuncList: 
-    print ln
+
+### make a list of all the unique words in the text file ###
+
+allWords = []
+
+for ln in lowerNoPuncList:
+    for word in ln.split():
+        allWords.append(word)
+
+uniqueWords = []
+
+for word in allWords:
+    imUnique = True
+    for uwrd in uniqueWords:
+        if(word == uwrd or word == "0" or word == "1"):
+            imUnique = False
+    if(imUnique):
+        uniqueWords.append(word)
 
 
+### alphabeticatize the list for easier debugging later ###
 
-### Form the vocabulary, make a list of all the unique words in the text file in alphabetical order. ###
+
+### Form the vocabulary ###
+
 
 ### Convert the training AND test data into a set of features based on the vocabulary. ###
 
