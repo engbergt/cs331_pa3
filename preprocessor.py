4263ef2ad2e600d504644e6b365132b61353d93a
Tarren Engberg <tarren@outlook.com>
2018-05-24 16:48:03 -0700

    refactored and built everything into functions.

--- preprocessor.py
+++ preprocessor.py
@@ -1,57 +1,66 @@
 import string
 
+def removePunctuation(arrayOfStrings):
+    noPuncList = []
+    for ln in arrayOfStrings: noPuncList.append(ln.translate(None, string.punctuation))
+    return noPuncList
+
+def lowercaseize(arrayOfStrings):
+    lowerCase = []
+    for ln in arrayOfStrings:lowerCase.append(ln.lower())
+    return lowerCase
+
+def makeListOFWords(arrayOfStrings):
+    allWords = []
+    for ln in arrayOfStrings:
+        for word in ln.split():allWords.append(word)
+    return allWords
+
+def uniquify(arrayOfStrings):
+    uniqueWords = []
+    for word in arrayOfStrings:
+        imUnique = True
+        for uwrd in uniqueWords:
+            if(word == uwrd or word == "0" or word == "1"):
+                imUnique = False
+        if(imUnique):
+            uniqueWords.append(word)
+    return uniqueWords
+
+def alphabetize(arrayOfStrings):
+    vocabulary = []
+    for word in sorted(arrayOfStrings): vocabulary.append(word)
+    return vocabulary
+
+
 ### Import file data ###
 
+with open('trainingSet.txt') as textFile:
+    trainingSet = textFile.readlines()
+
+with open('testSet.txt') as textFile:
+    testSet = textFile.readlines()
+
 with open('myTester.txt') as textFile:
-    content = textFile.readlines()
+    myTester = textFile.readlines()
 
 
 ### Remove punctuation & apostrophes ###
-
-noPuncList = []
-
-for ln in content: 
-    noPuncList.append(ln.translate(None, string.punctuation))
-
+trainingNoPunc = removePunctuation(trainingSet)
 
 ### Make all lowercase ###
+trainingLower = lowercaseize(trainingNoPunc)
 
-lowerNoPuncList = []
+### Make a list of all the unique words in the text file ###
+traingingWords = makeListOFWords(trainingLower)
+trainingUniqueWords = uniquify(traingingWords)
 
-for ln in noPuncList: 
-    lowerNoPuncList.append(ln.lower())
-
-
-### make a list of all the unique words in the text file ###
-
-allWords = []
-
-for ln in lowerNoPuncList:
-    for word in ln.split():
-        allWords.append(word)
-
-uniqueWords = []
-
-for word in allWords:
-    imUnique = True
-    for uwrd in uniqueWords:
-        if(word == uwrd or word == "0" or word == "1"):
-            imUnique = False
-    if(imUnique):
-        uniqueWords.append(word)
-
-
-### alphabeticatize the list for easier debugging later ###
-
-alphabetizedWords = []
-
-for word in sorted(uniqueWords):
-    alphabetizedWords.append(word)
-
-
-### Form the vocabulary ###
-
+### alphabatize the vocabulary for easier debugging later ###
+vocabulary = alphabetize(trainingUniqueWords)
 
 ### Convert the training AND test data into a set of features based on the vocabulary. ###
+
+
+
 
 ### Output the features into files called preprocessed_train.txt & preprocessed_test.txt each w/ the csv vocab at the top plus 'classlabel' ###
