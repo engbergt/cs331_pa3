24bedfde1aa345b86e8217f082f0b96bc081467e
Tarren Engberg <tarren@outlook.com>
2018-05-23 18:32:14 -0700

    test data file created, preprocessor steps outlined in comments

---
+++ preprocessor.py
@@ -0,0 +1,11 @@
+# Convert each sentance into a feature vector + class label.
+
+# Remove punctuation & apostrophes
+
+# Make all lowercase
+
+# Form the vocabulary, make a list of all the unique words in the text file in alphabetical order.
+
+# Convert the training AND test data into a set of features based on the vocabulary.
+
+# Output the features into files called preprocessed_train.txt & preprocessed_test.txt each w/ the csv vocab at the top plus 'classlabel'
